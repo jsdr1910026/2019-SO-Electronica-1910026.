67a63b70840bddf844966cfbfac44ca5f8f75d23
Gustavo Pinto <gustavo.pinto@correounivalle.edu.co>
2019-07-11 12:48:33 -0500

    adicionando memory-user

---
+++ 2019_07_11/memory-user.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int arg, char** argv){
+
+	if (arg !=2){
+	 printf("Por favor digitar un número\n");
+	 exit(-1);
+
+ }
+	int memorysize = atoi(argv[1]);
+	memorysize = memorysize*1024*1024;
+
+	int *arreglo = (int*) malloc(sizeof(int) * memorysize);
+	if (arreglo == NULL) {
+	 printf("Error en la asignación de memoria\n");
+	 exit(-1);
+}
+	int i=0;
+
+	for (; i < memorysize; i++) {
+	 arreglo[i] = i;
+ }
+ sleep(10);
+ free(arreglo);
+ return 0;
+
+}
